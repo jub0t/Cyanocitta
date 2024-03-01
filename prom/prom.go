373b401c824edc4cc9a0cb2e8eeb9489403c9055
jub0t <jforeverything2007@gmail.com>
2024-03-01 22:21:39 +0100

    Minor Update

--- prom/prom.go
+++ prom/prom.go
@@ -3,25 +3,11 @@ package prom
 import (
 	"disco/structs"
 	"os"
-	"syscall"
 )
 
 // prom - Custom PROcess Manager
-
 func GetRamUsage(proc *os.Process) (int64, error) {
-	// Wait for the process to collect its state
-	ps, err := proc.Wait()
-	if err != nil {
-		return 0, err
-	}
-
-	// Extract usage information from the process state
-	rss := ps.SysUsage().(*syscall.Rusage).Maxrss
-
-	// Convert RSS to bytes (on most systems, Maxrss is in kilobytes)
-	rssBytes := rss * 1024
-
-	return rssBytes, nil
+	return 10, nil
 }
 
 // StartInstance() => Start{Language}Instance()
